27ca69df17ca26c4682d91bb9c8944209f737974
utkarshyadav009 <57644363+utkarshyadav009@users.noreply.github.com>
2023-02-16 15:49:35 +0000

    Added plugin icon, experimenting with pfnn character class
    AAAAAAAAAAAAAAAHHHHHHHHH
    Nice

--- Source/UE5_PFNN/UE5_PFNNGameMode.cpp
+++ Source/UE5_PFNN/UE5_PFNNGameMode.cpp
@@ -7,9 +7,13 @@
 AUE5_PFNNGameMode::AUE5_PFNNGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/PFNN_BP/PFNN_Character/PFNNCharacter_BP"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
 	}
 }
+
+
+//"/Game/PFNN_BP/PFNN_Character/PFNNCharacter_BP"
+//"/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"
